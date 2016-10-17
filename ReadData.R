4e0d72e9f8dce95442a6e8c336f63ae4cf1b46f1
Konzert <HelloGitWorld@gmail.com>
2016-10-17 12:02:22 -0500

    Update ReadData.R

--- ReadData.R
+++ ReadData.R
@@ -1,3 +1,4 @@
+
 setwd("C:/Users/karlk/Desktop/EconometricsData/SpotifyData/")
 
 ldf <- list() # creates a list
@@ -5,16 +6,34 @@ listtxt <- dir(pattern = "*.csv")
 
 ldf = lapply(listtxt, function(x) {
   
-  dat = read.csv(x, header=FALSE)
+  dat = read.csv(x, header=TRUE)
+  
+  # Add column names
+  # names(dat) = c('Name', 'Sex', 'Number')
   
   # Add a column with the year
   dat$Date = substr(x,1,10)
-  
   return(dat)
 })
 
 library(dplyr)
-df = rbind_all(ldf)
+SpotifyDF = rbind_all(ldf)
 
 
-head(df)
+head(SpotifyDF)
+
+str(SpotifyDF)
+
+
+SpotifyDF <- data.frame(SpotifyDF)
+SpotifyDF$TrackID <- substr(SpotifyDF$URL, 32, 53)
+
+SpotifyTracks <- na.omit(SpotifyDF$TrackID)
+SpotifyTracks <- unique(SpotifyTracks)
+SpotifyTracks <- subset(SpotifyTracks, SpotifyTracks != "")
+SpotifyTracks <- data.frame(SpotifyTracks)
+
+
+
+rm(ldf)
+rm(listtxt)
